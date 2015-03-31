c254fa9e8ecc56429a19953370d5d2f9ea90041d
Leandro David Cacciagioni <leandro.21.2008@gmail.com>
2015-03-31 16:48:19 -0300

    First working mocker java

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
-${BP_URL:="https://github.com/cloudfoundry/java-buildpack.git"}
-${BP_VERSION:="v2.7.1"}
+
+if [ -z "$BP_URL" ]; then
+	export BP_URL="https://github.com/cloudfoundry/java-buildpack.git"
+else
+	echo "Buildpack defined by user"
+fi
+
+if [ -z "$BP_VERSION" ]; then
+	export BP_VERSION="v2.7.1"
+else
+	echo "Buildpack version defined by user"
+fi
+
 git clone $BP_URL /buildpack && cd /buildpack && git checkout $BP_VERSION && git submodule update --init --recursive
+
 # Here we detect if the file is zipped
-if [$(file -b /object | grep -i zip)]; then
-	unzip -o -K -d $HOME 
+if [ "$(file /object | grep -b -i zip | wc -l)" -eq 1 ];then
+	unzip -o -K /object -d $HOME
 fi
 cd $HOME
-/buildpack/bin/compile $HOME $TMPDIR
+/buildpack/bin/compile $HOME $TMPDIR && /buildpack/bin/release $HOME > /opt/release.yml
 if [ -d $HOME/.profile.d ]; then
 	for profile in $(ls $HOME/.profile.d); do
 		source $HOME/.profile.d/$profile
 	done
 fi
-$RUN_COMMAND
+/rel_parser.py /opt/release.yml
